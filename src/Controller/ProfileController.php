2eea39c3119a816cfbf3a983b898702b941e5be1
JakubSzczerba <jakszczerb@gmail.com>
2021-06-21 20:59:57 +0200

    Cosmetics in profile controller before crate a profile repository

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use App\Repository\ProfileRepository;
 use App\Entity\UserPreferention;
 use App\Entity\User;
 
@@ -16,7 +17,7 @@ class ProfileController extends AbstractController
     /**
    * @Route("/profile", name="profile")
    */
-  public function showProfile()
+  public function showProfile(Request $request, DashboardCaloriesRepository $profileRepository): Response
   {
     return $this->render('User/profile.html.twig', []);
   }
