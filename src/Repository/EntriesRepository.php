7be54b2585dd43cae789a7d919a7e84e4dd8d068
JakubSzczerba <jakszczerb@gmail.com>
2021-08-02 19:59:45 +0200

    create query for summing fats

--- src/Repository/EntriesRepository.php
+++ src/Repository/EntriesRepository.php
@@ -379,7 +379,26 @@ class EntriesRepository extends ServiceEntityRepository
 
     }
 
+    /**
+     * @return entried_Fats[]
+     */
+
+    public function SummEntriedFats(\DateTime $datetime, int $id)
+    {
+        
+        $qb = $this->createQueryBuilder('e');
+
+        $qb->select('SUM(e.fatXgram) as totalFats')
+            ->where('e.user = :user') 
+            ->andWhere('e.datetime = :datetime')   
+            ->setParameter('user', $id)
+            ->setParameter('datetime', $datetime->format('Y-m-d'));
+            
+        return $qb->getQuery()->getSingleScalarResult();
+
+    }
 
 
-}
+
+} 
 
