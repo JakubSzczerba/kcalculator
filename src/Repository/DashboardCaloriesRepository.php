e5dba8d9043958e16fa9d413ffd96350f73c91a0
JakubSzczerba <jakszczerb@gmail.com>
2021-08-02 19:37:59 +0200

    now everything work for proteins

--- src/Repository/DashboardCaloriesRepository.php
+++ src/Repository/DashboardCaloriesRepository.php
@@ -24,7 +24,7 @@ class DashboardCaloriesRepository extends ServiceEntityRepository
     {
         $qb = $this->createQueryBuilder('p');
 
-        $qb->select('p.kcal_day')
+        $qb->select('p.kcal_day, p.proteinPerDay')
             ->innerJoin('App\Entity\User', 'u', Join::WITH, 'u = p.users')
             ->where('p.kcal_day IS NOT NULL')
             ->andWhere('u.id like :users')
