ecc46759ff256ee0278254fdf5cba2f571ca4466
JakubSzczerba <jakszczerb@gmail.com>
2021-09-04 17:14:47 +0200

    create a new Entity for user's tracking  progress in weight

---
+++ src/Entity/UsersWeightHistory.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="users_entries")
+ */
+class UsersWeightHistory
+{
+}
+
+
