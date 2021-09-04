89d508c67dd9402d4c4f3c7d923ca54e4b33e25f
JakubSzczerba <jakszczerb@gmail.com>
2021-09-04 17:16:29 +0200

    id field

--- src/Entity/UsersWeightHistory.php
+++ src/Entity/UsersWeightHistory.php
@@ -6,10 +6,17 @@ use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity()
- * @ORM\Table(name="users_entries")
+ * @ORM\Table(name="users_weight_history)
  */
 class UsersWeightHistory
 {
+     /**
+   * @ORM\Column(type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   * 
+   */
+  private $id;
 }
 
 
