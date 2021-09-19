5bcea5fd75da1851bad8f419cca079ccc3c03024
JakubSzczerba <jakszczerb@gmail.com>
2021-09-19 19:02:24 +0200

    new entity - user weight history

---
+++ src/Entity/UserWeightHistory.php
@@ -0,0 +1,53 @@
+<?php
+namespace App\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="user_weight_history)
+ */
+class UsersWeightHistory
+{
+    /**
+   * @ORM\Column(type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   * 
+   */
+  private $id;
+
+  /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\UserPreferention", inversedBy="userWeightHistory")
+     * @ORM\JoinColumn(name="user_preferention_id", nullable=false, referencedColumnName="id")
+     */
+    private $userPreferention;
+
+  /**
+     * @var \DateTime
+     * 
+     * @ORM\Column(type="date") 
+     */
+    private $datetime;
+
+    /**
+     * @ORM\Column(type="float") 
+     */
+    private $userWeight;
+
+
+
+    public function __construct()
+    {
+        $this->data = new \DateTime();      
+    }
+
+
+
+
+
+
+
+
+}
