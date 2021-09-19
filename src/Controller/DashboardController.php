62620fad9ce5a4671edffce1af44c9144dedb711
JakubSzczerba <jakszczerb@gmail.com>
2021-09-19 17:30:48 +0200

    remove comments

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -54,15 +54,6 @@ class DashboardController extends AbstractController
             ],
         ]);
 
-        /*
-        abel: 'My First Dataset',
-    data: [300, 50, 100],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)'
-        */
-
 
 
     return $this->render('Homepage/homeafterlog.html.twig', [
