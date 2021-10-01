d2162c3939bde14fd5f9d731e4b955d9b2ed3b76
JakubSzczerba <jakszczerb@gmail.com>
2021-10-01 18:55:00 +0200

    think about refreshing pages

--- assets/app.js
+++ assets/app.js
@@ -23,4 +23,15 @@ $("#changeBGC").click(function(){
     $("body").css("background-color","black");
   })
 
+  $(document).ready(function(){
+    $("button").click(function(){
+      $("#searchingField").load("searchproducts.html.twig",  function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success")
+          alert("External content loaded successfully!");
+        if(statusTxt == "error")
+          alert("Error: " + xhr.status + ": " + xhr.statusText);
+      });
+    });
+  });
+
 
