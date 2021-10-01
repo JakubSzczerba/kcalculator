976d67f1edf7589ec38fa698c1afb6f9f21c3c76
JakubSzczerba <jakszczerb@gmail.com>
2021-10-01 19:20:42 +0200

    need to read about ajax methon in jquery

--- assets/app.js
+++ assets/app.js
@@ -23,15 +23,10 @@ $("#changeBGC").click(function(){
     $("body").css("background-color","black");
   })
 
-  $(document).ready(function(){
-    $("button").click(function(){
-      $("#searchingField").load("searchproducts.html.twig",  function(responseTxt, statusTxt, xhr){
-        if(statusTxt == "success")
-          alert("External content loaded successfully!");
-        if(statusTxt == "error")
-          alert("Error: " + xhr.status + ": " + xhr.statusText);
-      });
-    });
+$("button").click(function(){
+    $("#searchingField").load("User/searchproducts.html.twig");
+
   });
+  
 
 
