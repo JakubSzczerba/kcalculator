377ca395751659ebed392a24bf5ef964c8ba09eb
Jakub Szczerba <jakszczerb@gmail.com>
2021-11-04 22:34:12 +0100

    Refactoring all controller - Daily

--- src/Controller/DailyController.php
+++ src/Controller/DailyController.php
@@ -1,131 +1,111 @@
 <?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * It is part of an engineering project - copyright is reserved
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
 declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Twig\Extension\AbstractExtension;
-use Twig\TwigFilter;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use App\Repository\ProductRepository;
 use App\Repository\EntriesRepository;
-use App\Entity\User;
 use App\Entity\UsersEntries;
 use App\Entity\Products;
+use Symfony\Component\HttpFoundation\Request;
 
 class DailyController extends AbstractController
 {
+  private ProductRepository $productRepository;
+
+  private EntriesRepository $entriesRepository;
+
+  private EntityManagerInterface $entityManager;
+
+  public function __construct(
+    ProductRepository $productRepository,
+    EntriesRepository $entriesRepository,
+    EntityManagerInterface $entityManager
+  ) {
+    $this->productRepository = $productRepository;
+    $this->entriesRepository = $entriesRepository;
+    $this->entityManager = $entityManager;  
+  }
+
 /**
    * @Route("/daily", name="daily")
    */
   public function daily()
-  {
-    
-    return $this->render('User/daily.html.twig', []);
-    
+  {  
+    return $this->render('User/daily.html.twig');    
   }
-
 
 /**
    * @Route("/product", methods="POST", name="findFood")
    */
-  public function findFood(Request $request, ProductRepository $products): Response
+  public function findFood(Request $request): Response
   {
-    $nameproduct = $_POST["search"];
-    $foundProducts = $products->findProducts($nameproduct);
-    //$oneResult = $_GET["showThisProduct"];
-
-
+    $nameproduct = $request->get('search');
+    $foundProducts = $this->productRepository->findProducts($nameproduct);
+  
     return $this->render('User/searchproducts.html.twig',[
       'products' => $foundProducts
     ]);
-
-    $results = [];
-    
-    foreach ($foundProducts as $product) {
-      $results[]=[
-        'product' => $product->getProduct,
-        'energy' => $product->getEnergy,
-        'protein' => $product->getProtein,
-        'fat' => $product->getFat,
-        'carbo' => $product->getCarbo,
-
-      ];
-
-    }
-    return $results;
-      
-
   }
-
-  
 
   /**
    * @Route("/product/{id}", name="product.detail")
    */
-  public function showOneProduct(int $id, Products $product): Response
+  public function showOneProduct(Products $product): Response
   {
-  
     return $this->render('User/loadEntry.html.twig', [
       'product' => $product,
     ]);
-    
-    
-
   }
 
   /**
    * @Route("/product{id}", methods="POST", name="addEntry")
    */
-  public function addEntry(int $id, Request $request, EntityManagerInterface $entityManager, Products $product): Response
-  { 
-    
+  public function addEntry(Request $request, int $id, Products $product): Response
+  {   
     $em = $this->getDoctrine()->getManager();
 
     $meal_type = '';
     $grammage = '';
-    $energyXgram;
-    $proteinXgram;
-    $fatXgram;
-    $carboXgram;
-   
-    //Grammage
-
     
-    if(!empty($_POST['Meals'])) 
+    if(!empty($request->get('Meals')) || !empty($request->get('Grammage')) ) 
     {
-      $meal_type = $_POST['Meals'];
+      $meal_type = $request->get('Meals');
+      $grammage = $request->get('Grammage');
     } 
 
-    if(!empty($_POST['Grammage']))
-    {
-      $grammage = $_POST['Grammage'];
-    }
-    
+    // getting infroamtion about selected product
     $product = $em->getRepository(Products::class)->find($id);
-    
     $energy = $product->getEnergy();
     $protein = $product->getProtein();
     $fat = $product->getFat();
     $carbo = $product->getCarbo();
     
+    // recalculation of product's informations by selected grammage
     $energyXgram = $energy * $grammage;
     $proteinXgram = $protein * $grammage;
     $fatXgram = $fat * $grammage;
     $carboXgram = $carbo * $grammage;
 
+    // rounding results
     $energyXgram = round($energyXgram, 0);
     $proteinXgram = round($proteinXgram, 2);
     $fatXgram = round($fatXgram, 2);
     $carboXgram = round($carboXgram, 2);
-
     
+    // Creating Entry
     $entry = new UsersEntries();
-
     $entry->setUser($this->getUser());
     $entry->setDateTime(new \DateTime());
     $entry->setMealType($meal_type);
@@ -136,55 +116,49 @@ class DailyController extends AbstractController
     $entry->setFatXgram($fatXgram);
     $entry->setCarboXgram($carboXgram);
 
-    $entityManager->persist($entry);
-    $entityManager->flush();
-
-
+    $this->entityManager->persist($entry);
+    $this->entityManager->flush();
 
     if ($this->getUser()) 
-        {
-        return $this->redirectToRoute('showEntries');
-             
-        } else 
-        {
+      {
+        return $this->redirectToRoute('showEntries');            
+      } 
+
+      else {   
         return $this->render('User/daily.html.twig', [
           'product' => $product,
-        ]);
-        }
-
+          ]
+        );
+      }
   }
 
   /**
    * @Route("/wpisy/delete/{id}", name="deleteEntry")
    */
-  public function deleteEntry(Request $request, int $id, EntityManagerInterface $entityManager)
-  { 
-    
+  public function deleteEntry(int $id)
+  {     
     $entry = $this->getDoctrine()->getRepository(UsersEntries::class)->find($id);
 
     if ($id)
-    {
-      
-      $entityManager = $this->getDoctrine()->getManager();
-      $entityManager->remove($entry);
-      $entityManager->flush();
+    {    
+      $this->entityManager = $this->getDoctrine()->getManager();
+      $this->entityManager->remove($entry);
+      $this->entityManager->flush();
+
       return $this->redirectToRoute('showEntries');
     } 
     else {
       return $this->render('User/testEntry.html.twig', []);
     }
-
-
   }
   
  /**
    * @Route("/wpisy/edit/{id}",  methods="GET|POST", name="editEntry")
    */
-  public function editEntry(Request $request, int $id, EntityManagerInterface $entityManager)
+  public function editEntry(int $id)
   {
     $entry = new UsersEntries(); 
     $entry = $this->getDoctrine()->getRepository(UsersEntries::class)->find(array('id' => $id,));
-
 
     $meal_type = '';
     $grammage = $entry->getGrammage();
@@ -226,33 +200,28 @@ class DailyController extends AbstractController
     $entry->setFatXgram($fat);
     $entry->setCarboXgram($carbo);
 
+    $this->entityManager = $this->getDoctrine()->getManager();
+    $this->entityManager->flush();
     
-    
-  
-    $entityManager = $this->getDoctrine()->getManager();
-    $entityManager->flush();
-    
-
     if (isset($_POST['editbut']))
     {
       return $this->redirectToRoute('showEntries');
     } 
+
     else {
     return $this->render('User/editEntry.html.twig', [
-      'entry' => $entry,
-      
-    ]); }
-  
+      'entry' => $entry,     
+        ]
+      ); 
+    } 
   }
 
   /**
    * @Route("/wpisy",  methods="GET|POST", name="showEntries")
    */
-  public function showEntries(Request $request, EntriesRepository $entriesRepository): Response
+  public function showEntries(Request $request): Response
   {
     $id = $this->getUser()->getId();
-    $datetime;
-    $meal='';
     $meal1 = "Przekąska";
     $meal2 = "Śniadanie";
     $meal3 = "Drugie śniadanie";
@@ -260,40 +229,30 @@ class DailyController extends AbstractController
     $meal5 = "Podwieczorek";
     $meal6 = "Kolacja";
 
-    if (isset($_POST['dataTocheckDaily']))
+    if ($request->get('dataTocheckDaily'))
     { 
-      //echo "jest";
-      $datetime = new \DateTime($_POST['dataTocheckDaily']);
+      $datetime = new \DateTime($request->get('dataTocheckDaily'));
     }
+
     else {
-      //echo "nie jest";
       $datetime = new \DateTime('@'.strtotime('now'));
     }
 
-    $showEntry = $entriesRepository->displayEntry($datetime, $id); //all entries per day, but products are not grouping in one row (meal)
-
-    $ShowSnack = $entriesRepository->ShowSnack($datetime, $id, $meal1); // try to fetch all entries per day for row -> Przękąski 
-
-    $ShowBreakfast = $entriesRepository->ShowBreakfast($datetime, $id, $meal2); // the same way ^ but with Śnaidanie!
-
-    $ShowLunch = $entriesRepository->ShowLunch($datetime, $id, $meal3); // DRUGIE ŚNAIDANIE  dinner
-
-    $ShowDinner = $entriesRepository->ShowDinner($datetime, $id, $meal4); // OBIAD
-
-    $ShowTea = $entriesRepository->ShowTea($datetime, $id, $meal5); // Podwieczorek  
-
-    $ShowSupper = $entriesRepository->ShowSupper($datetime, $id, $meal6); // Koalcja 
+    $showEntry = $this->entriesRepository->displayEntry($datetime, $id); //all entries per day, but products are not grouping in one row (meal)
+    $ShowSnack = $this->entriesRepository->ShowSnack($datetime, $id, $meal1); // try to fetch all entries per day for row -> Przękąski 
+    $ShowBreakfast = $this->entriesRepository->ShowBreakfast($datetime, $id, $meal2); // the same way ^ but with Śnaidanie!
+    $ShowLunch = $this->entriesRepository->ShowLunch($datetime, $id, $meal3); // DRUGIE ŚNAIDANIE  dinner
+    $ShowDinner = $this->entriesRepository->ShowDinner($datetime, $id, $meal4); // OBIAD
+    $ShowTea = $this->entriesRepository->ShowTea($datetime, $id, $meal5); // Podwieczorek  
+    $ShowSupper = $this->entriesRepository->ShowSupper($datetime, $id, $meal6); // Koalcja 
 
     //summ kcal for meals near to name of meal
-    $SummSnacksKcal = $entriesRepository->SummSnacksKcal($datetime, $id, $meal1); // {{ snackcal|number_format }} 
-    $SummBreakfast = $entriesRepository->SummBreakfast($datetime, $id, $meal2);  //  {{ breakcal|number_format }} 
-    $SummLunch = $entriesRepository->SummLunch($datetime, $id, $meal3);         // {{ lunchkcal|number_format }} 
-    $SummDinner = $entriesRepository->SummDinner($datetime, $id, $meal4);      // {{ dinnerkcal|number_format }}
-    $SummTea = $entriesRepository->SummTea($datetime, $id, $meal5);           // {{ teakcal|number_format }}
-    $SummSupper = $entriesRepository->SummSupper($datetime, $id, $meal6);    // {{ supperkcal|number_format }}
-
-
-
+    $SummSnacksKcal = $this->entriesRepository->SummSnacksKcal($datetime, $id, $meal1); // {{ snackcal|number_format }} 
+    $SummBreakfast = $this->entriesRepository->SummBreakfast($datetime, $id, $meal2);  //  {{ breakcal|number_format }} 
+    $SummLunch = $this->entriesRepository->SummLunch($datetime, $id, $meal3);         // {{ lunchkcal|number_format }} 
+    $SummDinner = $this->entriesRepository->SummDinner($datetime, $id, $meal4);      // {{ dinnerkcal|number_format }}
+    $SummTea = $this->entriesRepository->SummTea($datetime, $id, $meal5);           // {{ teakcal|number_format }}
+    $SummSupper = $this->entriesRepository->SummSupper($datetime, $id, $meal6);    // {{ supperkcal|number_format }}
 
     return $this->render('User/testEntry.html.twig', [
       'entry' => $showEntry,
@@ -310,12 +269,7 @@ class DailyController extends AbstractController
       'teakcal' => $SummTea,
       'supperkcal' => $SummSupper,
       'dataTest' => $datetime,
-    ]);
-  
+      ]
+    );
   }
-
-
-
-
-
 }
