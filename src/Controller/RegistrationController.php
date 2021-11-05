6ba4d0366cc3ae8e88a2579a110070590956cf78
Jakub Szczerba <jakszczerb@gmail.com>
2021-11-05 21:21:59 +0100

    Refactoring all controller - Registration

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,6 +1,15 @@
 <?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * It is part of an engineering project - Kcalculator - copyright is reserved
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
 declare(strict_types=1);
+
 namespace App\Controller;
+
 use App\Entity\User;
 use App\Form\UserType;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
@@ -11,7 +20,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-
 class RegistrationController extends AbstractController
 {
     /**
@@ -21,6 +29,7 @@ class RegistrationController extends AbstractController
     {
         $form = $this->createForm(UserType::class);
         $form->handleRequest($request);
+
         if ($form->isSubmitted() && $form->isValid()) {
             $user = new User($form->get('username')->getData(), $form->get('email')->getData());
             $password = $passwordEncoder->encodePassword($user, $form->get('password')->getData());
@@ -28,14 +37,17 @@ class RegistrationController extends AbstractController
             try {
                 $entityManager->persist($user);
                 $entityManager->flush();
-                $session->getFlashBag()->add('success', sprintf('Account %s has been created!', $user->getUsername()));
+                $session->getFlashBag()->add('success', sprintf('Account %s has been created!', $user->getUsername()));            
                 return $this->redirectToRoute('home');
-            } catch (UniqueConstraintViolationException $exception) {
+            } 
+            catch (UniqueConstraintViolationException $exception) {
                 $session->getFlashBag()->add('danger', 'Email and username has to be unique');
             }
         }
+
         return $this->render('User/register.html.twig', [
             'form' => $form->createView()
-        ]);
+            ]
+        );
     }
 }
