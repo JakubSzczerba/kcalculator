a37360886a371d857a83c6aa8934b16464dd54c9
Jakub Szczerba <jakszczerb@gmail.com>
2021-11-05 21:19:43 +0100

    Refactoring all controller - Profile

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -1,60 +1,33 @@
 <?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * It is part of an engineering project - Kcalculator - copyright is reserved
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
 declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use App\Repository\ProfileRepository;
-use App\Entity\UserPreferention;
-use App\Entity\User;
 
 class ProfileController extends AbstractController
 {
     /**
    * @Route("/profile", name="profile")
    */
-  public function showProfile(Request $request, ProfileRepository $profileRepository): Response
+  public function showProfile(ProfileRepository $profileRepository)
   
   {
     $id = $this->getUser()->getId();
-
     $profile = $profileRepository->userProfile($id);
 
     return $this->render('User/profile.html.twig', [
         'profile' => $profile
-    ]);
-
-    
-
+      ]
+    );
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
