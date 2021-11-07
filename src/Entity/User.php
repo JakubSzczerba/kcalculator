e01c76af193330e5ffd2d07bf209d011fd24680d
Jakub Szczerba <jakszczerb@gmail.com>
2021-11-07 12:53:55 +0100

    Update of User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,45 +1,73 @@
 <?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * It is part of an engineering project - Kcalculator - copyright is reserved
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
+declare(strict_types=1);
+
 namespace App\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User implements UserInterface  {
+class User implements UserInterface  
+{
   /**
    * @ORM\Column(type="integer")
    * @ORM\Id
    * @ORM\GeneratedValue(strategy="AUTO")
    */
   private $id;
+
+  /**
+    * @var string
+    *
+    * @ORM\Column(type="string")
+  */
+  private $fullName;
+
   /**
    * @ORM\Column(type="string", length=128, unique=true)
    */
   private $username;
+
   /**
-   * @ORM\Column(type="string", length=128, unique=true) 
+   * @ORM\Column(type="string", length=128, unique=true)
    */
   private $email;
+
   /**
-   * @ORM\Column(type="string", length=4096, unique=true)
+   * @ORM\Column(type="string", length=4096)
    */
   private $password;
+
   /**
-     * @ORM\OneToOne(targetEntity="App\Entity\UserPreferention", mappedBy="users")            
-     */
-    private $preferentions;
+   * @var array
+   * 
+   * @ORM\Column(type="json")
+   */
+  private $roles = [];
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\UsersEntries", mappedBy="user")            
-     */
-    private $entry;
+  /**
+    * @ORM\OneToOne(targetEntity="App\Entity\UserPreferention", mappedBy="users")            
+    */
+  private $preferentions;
 
-    /**
-     * @ORM\OneToMany(targetEntity="App\Entity\UserWeightHistory", mappedBy="user")            
-     */
-    private $userWeightHistory;
+  /**
+    * @ORM\OneToMany(targetEntity="App\Entity\UsersEntries", mappedBy="user")            
+    */
+  private $entry;
 
-
+  /**
+    * @ORM\OneToMany(targetEntity="App\Entity\UserWeightHistory", mappedBy="user")            
+    */
+  private $userWeightHistory;
 
   /**
    * User constructor.
@@ -52,84 +80,123 @@ class User implements UserInterface  {
     $this->username = $username; 
     $this->email = $email;      
      // jesli zrobie drop db to musze usunac konstuktor, aby wczytac na naowo i zaladowac fixtures :)
-  }
-  
+  } 
  
   public function getId()
   {
-      return $this->id;
+    return $this->id;
+  }
+
+  public function setFullName(string $fullName): void
+  {
+    $this->fullName = $fullName;
+  }
+
+  public function getFullName(): ?string
+  {
+    return $this->fullName;
   }
   
-  public function getPreferention(): ?UserPreferention
-    {
-        return $this->preferentions;
-    }
-    public function setPreferention($preferentions): void
-    {
-        $this->preferentions = $preferentions;
-    }
+  /**
+   * Returns the roles or permissions granted to the user for security.
+   */
+  public function getRoles(): array
+  {
+    $roles = $this->roles;
 
-    public function getEntries(): ?UsersEntries
-    {
-        return $this->entry;
-    }
-    public function setEntries($entry): void
-    {
-        $this->entry = $entry;
-    }
+    if (empty($roles)) {
+      $roles[] = 'ROLE_USER';
+      }
 
-    
-
-  public function getRoles() {
-    return ['ROLE_USER'];
+    return array_unique($roles);
   }
+
+  public function setRoles(array $roles): void
+  {
+    $this->roles = $roles;
+  }
+
   public function getPassword(): string 
   {
-    return $this->password;
-    
+    return $this->password;  
   }
-  public function getSalt() {
-    // TODO: Implement getSalt() method.
+
+  /**
+   * Returns the salt that was originally used to encode the password.
+   * 
+   * {@inheritdoc}
+   */
+  public function getSalt(): ?string
+  {
+    // We're using bcrypt in security.yaml to encode the password, so
+    // the salt value is built-in and and you don't have to generate one
+    // See https://en.wikipedia.org/wiki/Bcrypt
+
+    return null;
   }
+
   public function getUsername(): string
   {
     return $this->username;
   }
+
   public function getEmail(): string
   {
     return $this->email;
   }
+
   public function eraseCredentials() {
     // TODO: Implement eraseCredentials() method.
   }
+
   /**
    * @param string $password
    */
   public function setPassword(string $password): self
   {
     $this->password = $password;
-    return $this;
-   
+    return $this;  
   }
+
   public function setUsername(string $username): self
     {
-        $this->username = $username;
-        return $this;
-    }
-    public function setEmail(string $email): self
-    {
-        $this->email = $email;
-        return $this;
+      $this->username = $username;
+      return $this;
     }
 
-    public function getUerWeightHistory(): ?UserWeightHistory
-    {
-        return $this->userWeightHistory;
-    }
-    public function setUerWeightHistory(?UserWeightHistory $userWeightHistory): self
-    {
-        $this->userWeightHistory = $userWeightHistory;
-    }
+  public function setEmail(string $email): self
+  {
+    $this->email = $email;
+    return $this;
+  }
 
-    
+  public function getPreferention(): ?UserPreferention
+  {
+    return $this->preferentions;
+  }
+
+  public function setPreferention($preferentions): void
+  {
+    $this->preferentions = $preferentions;
+  }
+
+  public function getEntries(): ?UsersEntries
+  {
+    return $this->entry;
+  }
+
+  public function setEntries($entry): void
+  {
+    $this->entry = $entry;
+  }
+
+  public function getUerWeightHistory(): ?UserWeightHistory
+  {
+    return $this->userWeightHistory;
+  }
+
+  public function setUerWeightHistory($userWeightHistory): void
+    {
+      $this->userWeightHistory = $userWeightHistory;
+    }
 }
