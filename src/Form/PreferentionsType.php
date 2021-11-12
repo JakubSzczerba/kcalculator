db55528b892db409deaa4fb846ee08d41f44431a
Jakub Szczerba <jakszczerb@gmail.com>
2021-11-12 20:51:12 +0100

    Last refactor form type and upgrade template for forms. Need to add saving datas for user weight history and next will be changing.

--- src/Form/PreferentionsType.php
+++ src/Form/PreferentionsType.php
@@ -39,17 +39,17 @@ class PreferentionsType extends AbstractType
             ])
             ->add('activity', ChoiceType::class, [
                 'choices' => [
-                    'Niska aktywność w ciągu dnia' => 'activity1',
-                    'Średnia aktywność w ciągu dnia' => 'activity2',
-                    'Wysoka aktywność w ciągu dnia' => 'activity3'
+                    'Siedzący tryb życia, praca przy biurku' => 'activity1',
+                    'Tryb życia o średniej aktywności fizczynej. Praca fizyczna, lub codziennie wykonywanie lekkich ćwiczeń fizycznych w czasie 1 godziny' => 'activity2',
+                    'Tryb życia o wysokiej aktywności fizczynej. Ciężka praca fizyczna, albo codzienny trening o wymiarze minimum 2 godzin z wysoką intesywnością' => 'activity3'
                 ],
                 'label' => 'Aktywność'
             ])
             ->add('intentions', ChoiceType::class, [
                 'choices' => [
-                    'Utrata wagi' => 'intension1',
-                    'Utrzymanie masy ciała' => 'intension2',
-                    'Budowa masy mięśniowej' => 'intension3'
+                    'Chcę spalić tkankę tłuszczową' => 'intension1',
+                    'Chcę utrzymać wagę' => 'intension2',
+                    'Chcę przybrać masy mięśniowej' => 'intension3'
                 ],
                 'label' => 'Intencje'
             ])
