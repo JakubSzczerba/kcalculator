d55b28975b324537fdba7b50f49b66cac5422e8a
Jakub Szczerba <jakszczerb@gmail.com>
2021-11-12 20:40:13 +0100

    Refactor all algorithm calculating calories. Extracting datas and preferentions by Form

--- src/Controller/PreferentionsController.php
+++ src/Controller/PreferentionsController.php
@@ -328,9 +328,123 @@ class PreferentionsController extends AbstractController
   /**
    * @Route("/testPref", name="testPref")
    */
-  public function testPref()
+  public function testPref(Request $request, EntityManagerInterface $entityManager)
   {
+    /** Reulst before fill from */
+    $result = 0;
+
+    /** Daily caloric requirement per user for */
+    $caloric_requirement = 0;
+
+    /** requirement Kcal according to established gains */
+    $kcal_day = 0; 
+    
+    /** Value connected with the chosen intents */
+    $burn = -300;           
+    $keep = 0;              
+    $gain = 300;            
+
+    /** Variable connected with amount of macronutrients per day - beefore calculating  */
+    $protein = 1;
+    $fat = 1;
+    $carbo = 1;
+
     $form = $this->createForm(PreferentionsType::class);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) 
+    {
+      $gender = $form->get('gender')->getData();
+      $weight = $form->get('weight')->getData();
+      $height = $form->get('height')->getData();
+      $age = $form->get('age')->getData();
+      $activity = $form->get('activity')->getData();
+      $intentions = $form->get('intentions')->getData();
+
+      if ($gender == 'man')
+      {
+        $gender = 'Mężczyzna';
+        $result = (10*$weight) + (6.25*$height) - (5*$age) + 5 ;
+      } 
+      elseif ($gender == 'woman')
+      {
+        $gender = 'Kobieta';
+        $result = (10*$weight) + (6.25*$height) - (5*$age) - 161 ;
+      }  
+
+      if ($activity == 'activity1')
+      {
+        $activity = 'niską aktywność w ciągu dnia';     //low activity
+        $result = $result * 1.45;      
+      }
+      elseif ($activity == 'activity2')
+      {
+        $activity = 'średnią aktywność w ciągu dnia';   //medium activity
+        $result = $result * 1.75;    
+      }
+      elseif ($activity == 'activity3')
+      {
+        $activity = 'wysoką aktywność w ciągu dnia';    //high activity
+        $result = $result * 2.0;        
+      } 
+      
+      settype($result, "integer");
+      $caloric_requirement = $caloric_requirement + $result;
+
+      if ($intentions == 'intension1')
+      {
+        $intentions = 'zredukować tkankę tłuszczową';
+        $kcal_day = ($caloric_requirement + $burn);
+
+        /** Macronutrients for burning fat */
+        $protein = 2 * $weight;
+        $fat = ($kcal_day * 0.25)/9 ;
+        $carbo = ($kcal_day - ($protein * 4) - ($fat * 9) )/4 ;
+      }
+      elseif ($intentions == 'intension2')
+      {
+        $intentions = 'utrzymać masę ciała';
+        $kcal_day = ($caloric_requirement + $keep);
+
+        /** Macronutrients for keep weight */
+        $protein = 1.60 * $weight;
+        $fat = ($kcal_day * 0.25)/9 ;
+        $carbo = ($kcal_day - ($protein * 4) - ($fat * 9) )/4 ;
+      }
+      elseif ($intentions == 'intension3')
+      {
+        $intentions = 'zbudować masę mięśniową';
+        $kcal_day = ($caloric_requirement + $gain);
+
+        /** Macronutrients for build muscle */
+        $protein = 1.85 * $weight; 
+        $fat = ($kcal_day * 0.25)/9 ;
+        $carbo = ($kcal_day - ($protein * 4) - ($fat * 9) )/4 ;
+      } 
+      
+      /** Changing type from few number after comma for just integer */
+      settype($kcal_day, "integer");
+      settype($protein, "integer");
+      settype($fat, "integer");
+      settype($carbo, "integer");
+     
+      $preferention = new UserPreferention();
+      $preferention->setGender($gender);
+      $preferention->setWeight($weight);
+      $preferention->setHeight($height);
+      $preferention->setAge($age);
+      $preferention->setActivity($activity);
+      $preferention->setKcal($caloric_requirement);
+      $preferention->setIntentions($intentions);
+      $preferention->setKcalDay($kcal_day);
+      $preferention->setProteinPerDay($protein);
+      $preferention->setFatPerDay($fat);
+      $preferention->setCarboPerDay($carbo);
+      $preferention->setUsers($this->getUser());
+
+      $entityManager->persist($preferention);
+      $entityManager->flush();
+    }
 
     return $this->render('User/testPref.html.twig', [
       'form' => $form->createView()
