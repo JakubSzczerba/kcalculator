5c6e2d6e0cf84ae6265b092387d51f49abbe9cf5
JakubSzczerba <jakszczerb@gmail.com>
2023-05-26 12:15:18 +0200

    Small uprade docker configuration and finish (?) controllers refactor

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -11,14 +11,13 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomepageController extends AbstractController
 {
-    /**
-     * @Route("/", name="home")
-     */
-    public function homepage()
+    #[Route('/', name: 'home')]
+    public function homepage(): Response
     {       
         if ($this->getUser()) 
         {
